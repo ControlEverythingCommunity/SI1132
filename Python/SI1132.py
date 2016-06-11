1a2672b74d88b667d46418385ef30cac9a985c38
Yadwinder Singh <ryker1990@gmail.com>
2016-06-11 20:12:22 +0530

    Update SI1132.py

--- Python/SI1132.py
+++ Python/SI1132.py
@@ -1,3 +1,9 @@
+// Distributed with a free-will license.
+// Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
+// SI1132
+// This code is designed to work with the SI1132_I2CS I2C Mini Module available from ControlEverything.com.
+// https://www.controleverything.com/content/Light?sku=SI1132_I2CS#tabs-0-product_tabset-2
+
 import smbus
 import time
 
@@ -15,7 +21,7 @@ bus.write_byte_data(0x60, 0x16, 0x00)
 bus.write_byte_data(0x60, 0x17, 0xF0)
 # SI1132 address, 0x60(96)
 # Select COMMAND register, 0x18(24)
-#		Select CHLIST register in RAM
+#		0x01 | 0xA0(161)   Select CHLIST register in RAM
 bus.write_byte_data(0x60, 0x18, (0x01 | 0xA0))
 time.sleep(0.01)
 # SI1132 address, 0x60(96)
@@ -23,15 +29,15 @@ time.sleep(0.01)
 status = bus.read_byte_data(0x60, 0x2E)
 
 # SI1132 address, 0x60(96)
-# Select INT Output Enable register
+# Select INT Output Enable register, 0x03(03)
 #		0x01(01)	INT pin driven low
 bus.write_byte_data(0x60, 0x03, 0x01)
 # SI1132 address, 0x60(96)
-# Select ALS Interrupt Enable register
+# Select ALS Interrupt Enable register, 0x04(04)
 #		0x01(01)	Assert INT pin whenever VIS or UV measurements are ready
 bus.write_byte_data(0x60, 0x04, 0x01)
 # SI1132 address, 0x60(96)
-# Select HW_KEY register
+# Select HW_KEY register, 0x07(07)
 #		0x17(23)	Default value
 bus.write_byte_data(0x60, 0x07, 0x17)
 
@@ -41,7 +47,7 @@ bus.write_byte_data(0x60, 0x07, 0x17)
 bus.write_byte_data(0x60, 0x17, 0x00)
 # SI1132 address, 0x60(96)
 # Select COMMAND register, 0x18(24)
-#		Select ALS_IR_ADCMUX register in RAM
+#		0x0E | 0xA0(174)    Select ALS_IR_ADCMUX register in RAM
 bus.write_byte_data(0x60, 0x18, (0x0E | 0xA0))
 time.sleep(0.01)
 # SI1132 address, 0x60(96)
@@ -54,7 +60,7 @@ status = bus.read_byte_data(0x60, 0x2E)
 bus.write_byte_data(0x60, 0x17, 0x00)
 # SI1132 address, 0x60(96)
 # Select COMMAND register, 0x18(24)
-#		Select ALS_IR_ADC_GAIN register in RAM
+#		0x1E | 0xA0(190)    Select ALS_IR_ADC_GAIN register in RAM
 bus.write_byte_data(0x60, 0x18, (0x1E | 0xA0))
 time.sleep(0.01)
 # SI1132 address, 0x60(96)
@@ -67,7 +73,7 @@ status = bus.read_byte_data(0x60, 0x2E)
 bus.write_byte_data(0x60, 0x17, 0x70)
 # SI1132 address, 0x60(96)
 # Select COMMAND register, 0x18(24)
-#		Select ALS_IR_ADC_COUNTER register in RAM
+#		0x1D | 0xA0(189)    Select ALS_IR_ADC_COUNTER register in RAM
 bus.write_byte_data(0x60, 0x18, (0x1D | 0xA0))
 time.sleep(0.01)
 # SI1132 address, 0x60(96)
@@ -80,7 +86,7 @@ status = bus.read_byte_data(0x60, 0x2E)
 bus.write_byte_data(0x60, 0x17, 0x00)
 # SI1132 address, 0x60(96)
 # Select COMMAND register, 0x18(24)
-#		Select ALS_VIS_ADC_GAIN register in RAM
+#		0x11 | 0xA0(177)    Select ALS_VIS_ADC_GAIN register in RAM
 bus.write_byte_data(0x60, 0x18, (0x11 | 0xA0))
 time.sleep(0.01)
 # SI1132 address, 0x60(96)
@@ -93,7 +99,7 @@ status = bus.read_byte_data(0x60, 0x2E)
 bus.write_byte_data(0x60, 0x17, 0x20)
 # SI1132 address, 0x60(96)
 # Select COMMAND register, 0x18(24)
-#		Select ALS_IR_ADC_MISC register in RAM
+#		0x1F | 0xA0(191)    Select ALS_IR_ADC_MISC register in RAM
 bus.write_byte_data(0x60, 0x18, (0x1F | 0xA0))
 time.sleep(0.01)
 # SI1132 address, 0x60(96)
@@ -106,7 +112,7 @@ status = bus.read_byte_data(0x60, 0x2E)
 bus.write_byte_data(0x60, 0x17, 0x70)
 # SI1132 address, 0x60(96)
 # Select COMMAND register, 0x18(24)
-#		Select ALS_VIS_ADC_COUNTER register in RAM
+#		0x10 | 0xA0(176)    Select ALS_VIS_ADC_COUNTER register in RAM
 bus.write_byte_data(0x60, 0x18, (0x10 | 0xA0))
 time.sleep(0.01)
 # SI1132 address, 0x60(96)
@@ -119,7 +125,7 @@ status = bus.read_byte_data(0x60, 0x2E)
 bus.write_byte_data(0x60, 0x17, 0x20)
 # SI1132 address, 0x60(96)
 # Select COMMAND register, 0x18(24)
-#		Select ALS_VIS_ADC_MISC register in RAM
+#		0x12 | 0xA0(178)    Select ALS_VIS_ADC_MISC register in RAM
 bus.write_byte_data(0x60, 0x18, (0x12 | 0xA0))
 time.sleep(0.01)
 # SI1132 address, 0x60(96)
@@ -138,8 +144,8 @@ time.sleep(0.5)
 data = bus.read_i2c_block_data(0x60, 0x22, 4)
 
 # Convert the data
-visible = (data[1] * 256 + data[0])
-ir = (data[3] * 256 + data[2])
+visible = data[1] * 256 + data[0]
+ir = data[3] * 256 + data[2]
 
 # SI1132 address, 0x60(96)
 # Read data back from 0x2C(44), 2 bytes
@@ -147,7 +153,7 @@ ir = (data[3] * 256 + data[2])
 data = bus.read_i2c_block_data(0x60, 0x2C, 2)
 
 # Convert the data
-uv = (data[1] * 256 + data[0])
+uv = data[1] * 256 + data[0]
 
 # Output data to screen
 print "Visible Light of Source : %d lux" %visible
